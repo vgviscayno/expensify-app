e132e5ff809cac7cc1b6843fdd999e30746b7378
Vonrie Viscayno <viscaynovonrie@gmail.com>
2021-08-31 00:51:08 +0800

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -17,25 +17,6 @@ import moment from 'moment';
 
 const store = configureStore();
 
-store.dispatch(
-    addExpense({
-        description: 'Water bill',
-        amount: 24000,
-        createdAt: moment(),
-    })
-);
-store.dispatch(
-    addExpense({ description: 'Gas bill', amount: 1000, createdAt: moment() })
-);
-store.dispatch(
-    addExpense({ description: 'Rent', amount: 24000, createdAt: moment() })
-);
-
-const state = store.getState();
-
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(state, visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
